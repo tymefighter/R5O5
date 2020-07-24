8104256d0c91add4c52d1d59819d5483f96f04da
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-07-24 15:46:38 +0530

    Remove comments

--- kernel/buffer.c
+++ kernel/buffer.c
@@ -1,27 +1,9 @@
-// Buffer cache.
-//
-// The buffer cache is a linked list of buf structures holding
-// cached copies of disk block contents.    Caching disk blocks
-// in memory reduces the number of disk reads and also provides
-// a synchronization point for disk blocks used by multiple processes.
-//
-// Interface:
-// * To get a buffer for a particular disk block, call bread.
-// * After changing buffer data, call bwrite to write it to disk.
-// * When done with the buffer, call brelse.
-// * Do not use the buffer after calling brelse.
-// * Only one process at a time can use a buffer,
-//         so do not keep them longer than necessary.
-
-
 #include "declarations.h"
 #include "functions.h"
 
 void binit(void) {
     Buffer *b;
 
-
-    // Create linked list of buffers
     bcache.head.prev = &bcache.head;
     bcache.head.next = &bcache.head;
     for(b = bcache.buf; b < bcache.buf+NBUF; b++){
@@ -32,13 +14,9 @@ void binit(void) {
     }
 }
 
-// Look through buffer cache for block on device dev.
-// If not found, allocate a buffer.
-// In either case, return locked buffer.
 static Buffer *bget(uint blockno) {
     Buffer *b;
 
-    // Is the block already cached?
     for(b = bcache.head.next; b != &bcache.head; b = b->next){
         if(b->blockno == blockno){
             b->refcnt++;
@@ -46,7 +24,6 @@ static Buffer *bget(uint blockno) {
         }
     }
 
-    // Not cached; recycle an unused buffer.
     for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
         if(b->refcnt == 0) {
             b->blockno = blockno;
@@ -55,33 +32,28 @@ static Buffer *bget(uint blockno) {
             return b;
         }
     }
+
     panic("bget: no buffers");
 }
 
-// Return a locked buf with the contents of the indicated block.
 struct Buffer *bread(uint blockno) {
-    Buffer *b;
+    Buffer *b = bget(blockno);
 
-    b = bget(blockno);
     if(!b->valid) {
         diskRW(b, 0);
         b->valid = 1;
     }
+
     return b;
 }
 
-// Write b's contents to disk.    Must be locked.
 void bwrite(Buffer *b) {
     diskRW(b, 1);
 }
 
-// Release a locked buffer.
-// Move to the head of the MRU list.
 void brelse(Buffer *b) {
-
     b->refcnt--;
     if (b->refcnt == 0) {
-        // no one is waiting for it.
         b->next->prev = b->prev;
         b->prev->next = b->next;
         b->next = bcache.head.next;
@@ -89,7 +61,6 @@ void brelse(Buffer *b) {
         bcache.head.next->prev = b;
         bcache.head.next = b;
     }
-
 }
 
 void bpin(Buffer *b) {
