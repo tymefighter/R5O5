ef879b0c3f0b8d631d8b69574134084a294e14eb
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-08-04 20:56:07 +0530

    abc

--- kernel/main.c
+++ kernel/main.c
@@ -4,7 +4,6 @@
 // Main - Here is where the control passes
 // from entry.S
 void main() {
-    w_satp(0);                      // Disable Paging
 
     consoleinit();                  // Set up Console
     printf("\nR5O5 kernel is booting\n\n");
@@ -21,7 +20,18 @@ void main() {
     \nfrom the starting of the log blocks\n");
     printf("done !\n");
 
-    createProcess(100, 0, 105, 895);
+    #define SATP_SV39 (8L << 60)
+    #define MAKE_SATP(pagetable) (SATP_SV39 | (((uint64)pagetable) >> 12))
+
+    uint64 addr = (uint64)kernelvec;
+    uint64 page = getPhyPage(addr);
+    pd[0].slotAllocated = True;
+    pd[0].sa.satp = allocatePageTable();
+    pd[0].sa.epc = addr;
+    pd[0].state = READY;
+    pd[0].sa.reg[1] = (allocatePage() << 12) + PGSIZE;
+    mapVirtualPage(pd[0].sa.satp, page, allocatePage(), True, True, True);
+    pd[0].sa.satp = (PageTable *)MAKE_SATP(pd[0].sa.satp);
     dispatcher();
 
     while(1)
